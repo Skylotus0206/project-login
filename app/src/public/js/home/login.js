41bcc1570525a989c4d00701722ba786ab9424aa
minha sohn <codeminhasohn@gmail.com>
2023-01-15 20:47:00 +0900

    use DOM object to exploit HTML obects

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,4 +1,18 @@
-//@ 프론트에서 작동하는 js
+//@ login page 에서 작동하는 js
+//@ 아이디와 패스워드를 브라우저에 입력했을 때, 그 정보가 서버로 request 하게 되고 이를 처리하는 과정이 서버가 해야한다
 'use strick';
 
-console.log('hello');
+//@DOM()
+const id = document.querySelector('#id');
+const password = document.querySelector('#password');
+const button = document.querySelector('button');
+
+button.addEventListener('click', login);
+
+function login() {
+  //@ 아이디와 비밀번호 값을 가져온다
+  const req = {
+    id: id.value,
+    password: password.value,
+  };
+}
